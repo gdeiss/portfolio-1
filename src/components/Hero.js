4a814692a8343172633f894449e4bae325dfaabc
gdeiss <52420516+gdeiss@users.noreply.github.com>
2021-08-20 13:26:09 -0400

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,8 +12,8 @@ var images = [ blackhole, snowflake, whirlpool, dunes ]
 
 var imageStyle = {
     flex: 1,
-    width: 180,
-    height: 180,
+    width: 140,
+    height: 140,
     resizeMode: 'contain'
 }
 
@@ -23,31 +23,31 @@ function Hero(props) {
 
   return(
     <Jumbotron>
-      <Container>
-        <Row>
-          <div class='col' style={{padding: "20px 0px 0px 40px"}}>
-            <img src={images[index]} style = {imageStyle} alt="fireSpot"/>
-          </div>
-          <div class='col-9'>
-            <Row>
-              <h2 style={{ color: 'white', fontSize:'3em', padding: "40px 0px 0px 0px" }}> Student @ Carnegie Mellon University</h2>
-            </Row>
-            <Row>
-              <h2 style={{ color: 'white', fontSize:'1.5em', padding: "0px 0px 0px 0px" }}> Statistics & Machine Learning</h2>
-            </Row>
-            <Row>
-              <h2 style={{ color: 'white', fontSize:'1.3em', padding: "0px 0px 0px 0px" }}> Game Design</h2>
-            </Row>
-          </div>
-        </Row>
+      <Container className="justify-content-md-center">
+          <Row className="justify-content-md-center">
+            <div class='col' style={{padding: "20px 0px 0px 40px", position : "relative"}}>
+              <img src={images[index]} style = {imageStyle} alt="fireSpot"/>
+            </div>
+            <div class='col-9'>
+              <Row>
+                <h2 style={{ color: 'white', fontSize:'2em', padding: "40px 0px 0px 0px" }}> Student @ Carnegie Mellon University</h2>
+              </Row>
+              <Row>
+                <h2 style={{ color: 'white', fontSize:'1.25em', padding: "0px 0px 0px 0px" }}> Statistics & Machine Learning</h2>
+              </Row>
+              <Row>
+                <h2 style={{ color: 'white', fontSize:'1.2em', padding: "0px 0px 0px 0px" }}> Game Design</h2>
+              </Row>
+            </div>
+          </Row>
         <Row>
           <p style={{ color: 'white', fontSize:'1em', padding: "30px 50px 0px 0px", marginLeft :"40px" }}>     I am a student at Carnegie Mellon University studying Statistics & Machine Learning with a minor in Game Design. You may have read that above. </p>
         </Row>
         <Row>
-          <p style={{ color: 'white', fontSize:'1em', padding: "10px 50px 0px 0px", marginLeft :"40px" }}>     Outside of schoolwork I enjoy music, games, movies, and volunteer work. </p>
+          <p style={{ color: 'white', fontSize:'1em', padding: "10px 50px 0px 0px", marginLeft :"40px" }}>     Outside of schoolwork I enjoy music, games, reading, and volunteer work. </p>
         </Row>
         <Row>
-          <p style={{ color: 'white', fontSize:'1em', padding: "10px 50px 0px 0px", marginLeft :"40px" }}>     Outside of music, games, movies, and volunteer work, I enjoy schoolwork. My chief interest lies in Data Science. I love the blend of the technical aspects of Machine Learning, modelling, etc. with the more creative process of visualizing data and writing a report. </p>
+          <p style={{ color: 'white', fontSize:'1em', padding: "10px 50px 0px 0px", marginLeft :"40px" }}>     Outside of music, games, reading, and volunteer work, I enjoy schoolwork. My chief interest lies in Data Science. I love the blend of the technical aspects of Machine Learning, modelling, etc. with the more creative process of visualizing data and writing a report. </p>
         </Row>
         <Row>
           <p style={{ color: 'white', fontSize:'1em', padding: "10px 50px 0px 0px", marginLeft :"40px" }}>     My primary languages are Python, R, and English, but I'm also familiar with Java, SQL, and just enough Javascript to make this website. I'm always excited to learn about more languages, libraries, and tools. </p>
